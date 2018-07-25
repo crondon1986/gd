ccfa49643d5025dbc104b87cb4a9873025ba99b8
crondon1986 <crondon1986@gmail.com>
2018-07-24 22:13:19 -0400

    cambios en el diseño del login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,16 +1,41 @@
+
 @extends('layouts.app')
 @section('content')
-<div class="login-box">
+<style type="text/css">
+  .fondo{
+    background-color: #000000 !important;
+    background-image: url('{{asset('/image/banner7.jpg')}}');
+  }
+  .login-box{
+    width: 480px;
+    margin: 7% auto;
+  }
+  .form-horizontal .form-group{
+    margin-right: 30px;
+    margin-left: 30px;
+  }
+</style>
+
+<div class="login-box  ">
     <div class="login-logo">
  
         
     </div>
-    <div class="login-box-body">
-       <p> <img src="{{asset('/file/foto/admin.png')}}" height="120" width="120"/> 
-          <a href="#"><b> Gestión de Documentos</b></a></p>
+    <div class="login-box-body">        <div>
+            <div class=""> 
+                <img src="{{asset('/image/banner7.jpg')}}" height="120" width="400"/>
+          </div>
+        </div>
+       <div class="login-logo"> <img src="{{asset('/image/logo-sistema13.jpg')}}" height="150" width="150"/> 
+          <!--<a href="#"><b> Gestión de Documentos</b></a>-->
+          </div>
         <p class="login-box-msg">Iniciar sesión</p>
+        <div class="col-md-6">
+            
+        </div>
         <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
             {{ csrf_field() }}
+
             <div class="form-group has-feedback{{ $errors->has('email') ? ' has-error' : '' }}">
                 <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
                 <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
